46593acd91e1e765979e6aa821fe593fb0d4e8e7
Ranu Jain <65921042+RanuJain131@users.noreply.github.com>
2024-05-23 03:04:11 +0530

    Update main.tf

--- aks-ranu/main.tf
+++ aks-ranu/main.tf
@@ -1,3 +1,25 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.48.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+  subscription_id            = "c61ea177-3092-49b2-b416-2742300b59fd"
+  client_id                  = "d2eb23fb-162a-4198-87c8-16e573d59205"
+  client_secret              = "1575bd5f-c65a-47c3-8ff6-1d7c90c94e90"
+  tenant_id                  = "d7d3f2c0-a6ee-4480-8526-ea05647335b3
+  skip_provider_registration = true
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "learnk8sResourceGroup"
+  location = "northeurope"
+}
+
 resource "azurerm_kubernetes_cluster" "cluster" {
   name                = "learnk8scluster"
   location            = azurerm_resource_group.rg.location
