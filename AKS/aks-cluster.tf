64a2d7cae837ff4584653568ba1761649c6283d2
Ranu Jain <65921042+RanuJain131@users.noreply.github.com>
2024-05-23 02:55:01 +0530

    Update aks-cluster.tf

--- AKS/aks-cluster.tf
+++ AKS/aks-cluster.tf
@@ -3,9 +3,55 @@
 
 resource "random_pet" "prefix" {}
 
+# Copyright (c) HashiCorp, Inc.
+# SPDX-License-Identifier: MPL-2.0
+
+resource "random_pet" "prefix" {}
+
 provider "azurerm" {
   features {}
+  subscription_id            = "c61ea177-3092-49b2-b416-2742300b59fd"
+  client_id                  = "d2eb23fb-162a-4198-87c8-16e573d59205"
+  client_secret              = "1575bd5f-c65a-47c3-8ff6-1d7c90c94e90"
+  tenant_id                  = "d7d3f2c0-a6ee-4480-8526-ea05647335b3
+  skip_provider_registration = true
 }
+
+resource "azurerm_resource_group" "default" {
+  name     = "${random_pet.prefix.id}-rg"
+  location = "West US 2"
+
+  tags = {
+    environment = "Demo"
+  }
+}
+
+resource "azurerm_kubernetes_cluster" "default" {
+  name                = "${random_pet.prefix.id}-aks"
+  location            = azurerm_resource_group.default.location
+  resource_group_name = azurerm_resource_group.default.name
+  dns_prefix          = "${random_pet.prefix.id}-k8s"
+  kubernetes_version  = "1.26.3"
+
+  default_node_pool {
+    name            = "default"
+    node_count      = 2
+    vm_size         = "Standard_D2_v2"
+    os_disk_size_gb = 30
+  }
+
+  service_principal {
+    client_id     = var.appId
+    client_secret = var.password
+  }
+
+  role_based_access_control_enabled = true
+
+  tags = {
+    environment = "Demo"
+  }
+}
+
 
 resource "azurerm_resource_group" "default" {
   name     = "${random_pet.prefix.id}-rg"
