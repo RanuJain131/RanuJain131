f3e1814864be63738015517019d46e70acc06904
Ranu Jain <65921042+RanuJain131@users.noreply.github.com>
2024-07-11 13:05:21 +0530

    Create provider.tf

---
+++ 2-Tier/provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    morpheus = {
+      source = "gomorpheus/morpheus"
+      version = "0.9.9"
+    }
+  }
+}
+
+provider "morpheus" {
+  # Configuration options
+}
